dd5ae51c935f9254d5d8914aeb098cfbd525a0ed
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-02-05 14:12:46 +0100

    AccountComponent created.

--- src/main/java/se/iths/fredrik/simplebankapplabb/component/AccountComponent.java
+++ src/main/java/se/iths/fredrik/simplebankapplabb/component/AccountComponent.java
@@ -1,4 +1,18 @@
 package se.iths.fredrik.simplebankapplabb.component;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AccountComponent {
+    private int balance = 0;
+
+    public void deposit(int amount){
+        balance += amount;
+    }
+    public void withdraw(int amount){
+        balance -= amount;
+    }
+    public int getBalance(){
+        return balance;
+    }
 }
