86993b0ee6aaa6a603c36b67110adea4b1cea69a
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-02-05 15:55:12 +0100

    ATMServiceTest exception tests created.

---
+++ src/test/java/se/iths/fredrik/simplebankapplabb/service/ATMServiceTest.java
@@ -0,0 +1,49 @@
+package se.iths.fredrik.simplebankapplabb.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.iths.fredrik.simplebankapplabb.component.AccountComponent;
+import se.iths.fredrik.simplebankapplabb.exception.InsufficientFundsException;
+import se.iths.fredrik.simplebankapplabb.exception.InvalidAmountException;
+import se.iths.fredrik.simplebankapplabb.exception.MaxWithdrawExceededException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ATMServiceTest {
+
+    @Mock
+    private AccountComponent account;
+
+    @InjectMocks
+    private ATMService atmService;
+
+    @Test
+    @DisplayName("Deposit throws InvalidAmountException")
+    void depositInvalidAmount(){
+        assertThrows(InvalidAmountException.class,
+                () -> atmService.deposit(0));
+    }
+
+    @Test
+    @DisplayName("Withdraw throws MaxWithdrawExceededException")
+    void  withdrawInvalidAmount(){
+        assertThrows(MaxWithdrawExceededException.class,
+                () -> atmService.withdraw(2000));
+    }
+
+    @Test
+    @DisplayName("Withdraw Throws InsufficientFundsException")
+    void withdrawInsufficientFunds(){
+        when(account.getBalance()).thenReturn(100);
+
+        assertThrows(InsufficientFundsException.class,
+                () -> atmService.withdraw(200));
+    }
+}
