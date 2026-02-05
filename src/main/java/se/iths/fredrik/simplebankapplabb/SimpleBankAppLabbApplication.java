1e03c9d5b96d83036a56700eae06d78379c75226
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-02-05 14:08:30 +0100

    initial commit

---
+++ src/main/java/se/iths/fredrik/simplebankapplabb/SimpleBankAppLabbApplication.java
@@ -0,0 +1,13 @@
+package se.iths.fredrik.simplebankapplabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankAppLabbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankAppLabbApplication.class, args);
+    }
+
+}
